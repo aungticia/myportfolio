e4b624405c9edcd25b04be2c9b063108a5118b85
Aung <104800264+aungoo3@users.noreply.github.com>
2024-03-17 23:33:25 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+
+// filter using javascript
+$(document).ready(function () {
+    $(".filter-item").click(function () {
+      const value = $(this).attr("data-filter");
+      if (value == "all") {
+        $(".post").show("1000");
+      } else {
+        $(".post")
+          .not("." + value)
+          .hide("1000");
+        $(".post")
+          .filter("." + value)
+          .show("1000");
+      }
+    });
+  });
+  
+  
+  // javascript for sticky navbar even if u scroll the navbar will be fixed
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar-top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar-top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
+  
+  
+  // adding funtionality to back to top button 
+  
+  //Get the button
+  let mybutton = document.getElementById("btn-back-to-top");
+  
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click",function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+  
+  // handle the form submission
+  // function sendEmail() {
+  //   Email.send({
+  //       Host : "smtp.gmail.com",
+  //       Username : "aungtechpro@gmail.com",
+  //       Password : "Tech20238@",
+  //       To : 'aungtechpro@gmail.com',
+  //       From : document.getElementById("email").value,
+  //       Subject : "New Contact Form Inquiry",
+  //       Body : "Name: " + document.getElementById("name").value 
+  //       + "<br>Email: " + document.getElementById("email").value 
+  //       + "<br>Phone no: " + document.getElementById("phone").value 
+  //       + "<br>Message: " + document.getElementById("message").value
+  //   }).then(
+  //       message => {
+  //         alert("Message Sent Successfully");
+  //         document.querySelector('.contact-form').reset(); 
+  //     }
+  //   );
+  // }
+  
+  
+  
+  
+  
+  
+  
